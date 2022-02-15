71df8f0d265b2b4b194ad7a0dbc034d86984151b
“Summer” <summerzoummg@gmail.com>
2022-02-14 23:31:28 -0500

    build html search bar and ul container

--- src/components/AssemblyLine.js
+++ src/components/AssemblyLine.js
@@ -1,7 +1,15 @@
 export default function AssemblyLine({ stages }) {
+    let content
     return (
         <div className="container">
-            AssemblyLine
+            <form className="search-box">
+                <label htmlFor="add-item">Add an item: </label>
+                <input
+                type="text"
+                id="add-item"
+                />
+            </form>
+            <div className="ul-container">{content}</div>
         </div>
     )
 }
