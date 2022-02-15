88e422855aae476b9d8ca93e37a775047fb5227e
“Summer” <summerzoummg@gmail.com>
2022-02-15 00:17:29 -0500

    item is able to detect left and right click now
    item is able to detect left and right click now.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,15 @@
 export default function Item({ item }) {
+    const handleClick = (e) => {
+        if (e.type === "click") {
+          console.log('left clicked')
+        } else if (e.type === "contextmenu") {
+          console.log('right clicked')
+        }
+    };
+
     return (
-        <li className="item">
-            Single Item
+        <li className="item" onClick={handleClick} onContextMenu={handleClick}>
+            {item.item}
         </li>
     )
 }
