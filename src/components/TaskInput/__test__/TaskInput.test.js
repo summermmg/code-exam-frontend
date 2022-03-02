907e5ede8f4e92f152a420069523b5a0f1350a26
“Summer” <summerzoummg@gmail.com>
2022-03-02 10:53:19 -0500

    add more tests to 'TaskInput'
    should have empty input after form is submitted

--- src/components/TaskInput/__test__/TaskInput.test.js
+++ src/components/TaskInput/__test__/TaskInput.test.js
@@ -3,6 +3,12 @@ import TaskInput from "../TaskInput";
 
 const mockedSetItemsData = jest.fn();
 
+const makeInput = (input) => {
+    const inputElement = screen.getByLabelText(/add an item:/i);
+    fireEvent.click(inputElement);
+    fireEvent.change(inputElement, { target: { value: input }});
+}
+
 describe('TaskInput', () => {
     test('should render input element', () => {
         render(
@@ -11,7 +17,7 @@ describe('TaskInput', () => {
                 setItemsData={mockedSetItemsData}
             />
         );
-        const inputElement = screen.getByLabelText(/add an item:/i)
+        const inputElement = screen.getByLabelText(/add an item:/i);
         expect(inputElement).toBeInTheDocument();
     })
 
@@ -23,8 +29,7 @@ describe('TaskInput', () => {
             />
         );
         const inputElement = screen.getByLabelText(/add an item:/i);
-        fireEvent.click(inputElement);
-        fireEvent.change(inputElement, { target: { value: "build a new sign-up feature" }});
+        makeInput("build a new sign-up feature");
         expect(inputElement.value).toBe("build a new sign-up feature");
     })
 
@@ -35,11 +40,23 @@ describe('TaskInput', () => {
                 setItemsData={mockedSetItemsData}
             />
         );
-        const inputElement = screen.getByLabelText(/add an item:/i);
-        fireEvent.click(inputElement);
-        fireEvent.change(inputElement, { target: { value: "build a new sign-up feature" }});
+        makeInput("build a new sign-up feature");
         const formElement = screen.getByRole('form', { name: ""});
         fireEvent.submit(formElement);
         expect(mockedSetItemsData).toBeCalled();
+    })
+
+    test('should have empty input after form is submitted', () => {
+        render(
+            <TaskInput 
+                stages={["Idea", "Development", "Testing", "Deployment"]}
+                setItemsData={mockedSetItemsData}
+            />
+        );
+        const inputElement = screen.getByLabelText(/add an item:/i);
+        makeInput("build a new sign-up feature");
+        const formElement = screen.getByRole('form', { name: ""});
+        fireEvent.submit(formElement);
+        expect(inputElement.value).toBe("");
     })
 })
